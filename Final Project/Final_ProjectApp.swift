68cd4b56d8011906ef010a7a9c73a968a86ac284
Carson Payne <carson.j.payne14@gmail.com>
2024-11-19 10:13:45 -0600

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Carson Payne on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final ProjectTests/Final_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectTests.swift
+//  Final ProjectTests
+//
+//  Created by Carson Payne on 11/19/24.
+//
+
+import Testing
+@testable import Final_Project
+
+struct Final_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
