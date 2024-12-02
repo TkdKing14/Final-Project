effc9c641b128e0201585bccb251ecfbb042e4e4
Carson Payne <carson.j.payne14@gmail.com>
2024-12-01 19:07:41 -0600

    Computer Transfer (Added Casino View)

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -10,32 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("Outside Casino")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-                .scaledToFill()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
-            Text("Slot Simulator")
-                .foregroundColor(.white)
-                .font(.largeTitle).bold()
-                .position(x:150,y:8)
-            
-            VStack {
-                Spacer()
-                Button(action: {
-                }) {
-                    Text("Enter Casino")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.black)
-                        .cornerRadius(10)
+        NavigationView {
+            ZStack {
+                Image("Outside Casino")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                Text("Slot Simulator")
+                    .foregroundColor(.white)
+                    .font(.largeTitle).bold()
+                    .position(x:150,y:8)
+                
+                VStack {
+                    Spacer()
+                    NavigationLink(destination: CasinoView()) {
+                        Text("Enter Casino")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                        
+                    }
+                   
                 }
-                .padding()
             }
         }
     }
