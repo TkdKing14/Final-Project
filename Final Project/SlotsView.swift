40bcf81c1fce47623a4b2c4b19b1c784d8564acf
Carson Payne <carson.j.payne14@gmail.com>
2024-12-09 16:30:34 -0600

    Fixed Border to be added when slots finish

--- Final Project/SlotsView.swift
+++ Final Project/SlotsView.swift
@@ -15,6 +15,9 @@ struct SlotsView: View {
     @State private var randomValue2 = 0                         //privides the main bases of the variable randomvalue1
     @State private var rotation = 0.0                           //privides the main bases of the variable rotation
     @State private var gameOver = false                         //privides the main bases of the variable randomvalue1
+    @State private var borderWidth0 = 0                        //privides the main bases of the variable randomvalue1
+    @State private var borderWidth1 = 0                        //privides the main bases of the variable randomvalue1
+    @State private var borderWidth2 = 0                        //privides the main bases of the variable randomvalue1
     var body: some View {                                       //this sets up another view the viewer can access
         ZStack {                                                //aligns both axises when considering the following code
             Image("Slot Machine 2")                             //adds "slot machine 2" image to the app
@@ -32,72 +35,72 @@ struct SlotsView: View {
             Image("pips\(randomValue0)")
                 .resizable()
                 .frame(width: 90, height: 90)
-                .border(.blue,width:4)
+                .border(.blue,width: CGFloat(borderWidth0))
                 .position(x:130, y:350)
-                
+            
                 .padding()
             Image("pips\(randomValue1)")
                 .resizable()
                 .frame(width: 90, height: 90)
-                .border(.yellow,width:4)
+                .border(.yellow,width:CGFloat(borderWidth1))
                 .position(x:230, y:350)
-               
+            
                 .padding()
             Image("pips\(randomValue2)")
                 .resizable()
                 .frame(width: 90, height: 90)
-                .border(.red,width:4)
+                .border(.red,width:CGFloat(borderWidth2))
                 .position(x:330, y:350)
                 .padding()
-                
-            //dif color boarder
             HStack {
                 Button("Spin") {
                     chooseRandom0(times: 5)
                     chooseRandom1(times: 8)
                     chooseRandom2(times: 11)
-                    }
-                    .position(x:112, y:484)
-                    .font(.title)           // makes "spin" text a title
-                    .fontWeight(.bold)      // makes "spin" text bold
-                    .padding()              //moves "spin" up
-                    .foregroundColor(.white)    // makes "spin" text white
-                    .cornerRadius(10)       // makes "spin" background rounded
-                    
                 }
+                .position(x:112, y:484)
+                .font(.title)           // makes "spin" text a title
+                .fontWeight(.bold)      // makes "spin" text bold
+                .padding()              //moves "spin" up
+                .foregroundColor(.white)    // makes "spin" text white
+                .cornerRadius(10)       // makes "spin" background rounded
+                
+            }
         }
     }
     func chooseRandom0(times: Int) {
         if times > 0 {
+            borderWidth0 = 0
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 randomValue0 = Int.random(in: 1...13)
                 chooseRandom0(times: times - 1)
             }
         } else {
-            //jvg
+            borderWidth0 = 4
         }
     }
     func chooseRandom1(times: Int) {
         if times > 0 {
+            borderWidth1 = 0
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 randomValue1 = Int.random(in: 1...13)
                 chooseRandom1(times: times - 1)
-                
-                
             }
+        } else {
+            borderWidth1 = 4
         }
     }
     func chooseRandom2(times: Int) {
         if times > 0 {
+            borderWidth2 = 0
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 randomValue2 = Int.random(in: 1...13)
                 chooseRandom2(times: times - 1)
-                
-                
             }
+        } else {
+            borderWidth2 = 4
         }
     }
-}
     struct CustomText: View {
         let text: String
         var body: some View {
@@ -115,7 +118,7 @@ struct SlotsView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 10))
         }
     }
-
+}
 #Preview {
     SlotsView()
 }
